738f63e6bd1463dbee859ee8290db687d6ef6399
mhahaha <727156131@qq.com>
2021-07-06 23:00:13 +0800

    update: 冒泡排序

--- bubble-sort/index.js
+++ bubble-sort/index.js
@@ -5,7 +5,7 @@
 //     while (count > 0) {
 //         for (let i = 0; i < count; i++) {
 //             let temp
-            
+
 //             if (arr[i] > arr[i + 1]) {
 //                 temp = arr[i]
 //                 arr[i] = arr[i + 1]
@@ -43,3 +43,40 @@ function bubbleSort(arr) {
 const arr = [2, 1, 5, 7, 3, 9, 0]
 
 console.log(bubbleSort(arr))
+
+function bubbleSort(arr, shouldChange) {
+
+    let length = arr.length,
+        count = length - 1;
+
+    while (count > 0) {
+        for (let i = 0; i < count; i++) {
+            let temp;
+
+            // 支持自定义交换条件
+            if (shouldChange.call(this, arr[i], arr[i + 1])) {
+                temp = arr[i]
+                arr[i] = arr[i + 1]
+                arr[i + 1] = temp
+            }
+        }
+
+        count--;
+    }
+
+    return arr;
+}
+
+let objArr = [
+    { value: 2, index: 1 },
+    { value: 5, index: 3 },
+    { value: 3, index: 6 },
+    { value: 9, index: 5 },
+    { value: 3, index: 2 }
+];
+
+// 首先根据value进行排序，value相等则根据index进行排序
+bubbleSort(objArr, (pre, next) => {
+    return pre.value > next.value ||
+          (pre.value === next.value && pre.index > next.index);
+});
